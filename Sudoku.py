2692a5e3afe083bb267d7a9f99cc439351257b48
rmj-code <ryanmartynjackson@gmail.com>
2021-04-21 20:01:00 +0100

    finished is_valid function

--- Sudoku.py
+++ Sudoku.py
@@ -23,11 +23,11 @@ def solve(bo):
 
 
 
-def is_valid(num,grid,row_n,col_n):
-	row_list = grid[row_n]					# Get Row as list
-	col_list = list(zip(*grid))[col_n]		# Get Col as list
-	mini_grid_r = int(row_n/3)*3	# Get mini grid (row,col)
-	mini_grid_c = int(col_n/3)*3	# Get mini grid (row,col)
+def is_valid(grid, num,row_col):
+	row_list = grid[row_col[0]]					# Get Row as list
+	col_list = list(zip(*grid))[row_col[1]]		# Get Col as list
+	mini_grid_r = int(row_col[0]/3)*3	# Get mini grid (row,col)
+	mini_grid_c = int(row_col[1]/3)*3	# Get mini grid (row,col)
 	mini_grid = list(rws[mini_grid_c:mini_grid_c+3] for rws in grid[mini_grid_r:mini_grid_r+3])
 
 	print("num: ",num)
@@ -38,18 +38,16 @@ def is_valid(num,grid,row_n,col_n):
 	print("mini_grid: ",mini_grid)
 
 
-
-
 	if (num in row_list):	# check if in rows
-		return f"Exists in row {row_n}: {row_list}"
+		return False			# f"Exists in row {row_col[0]}: {row_list}"
 	elif (num in col_list):	# check if in columns
-		return f"Exists in col {col_n}: {col_list}"
+		return False			# f"Exists in col {row_col[1]}: {col_list}"
 	elif (num in list(x for rw in mini_grid for x in rw)):
-		return f"Exists in mini_grid {mini_grid}"
+		return False			# f"Exists in mini_grid {mini_grid}"
 	else:
-		return "Free"
+		return True
 
-print (check_num(num=4,grid=grid, row_n=0, col_n=0))
+print (is_valid(grid=grid,num=4, row_col=(0,0)))
 
 # def check_row(num, row_list)
 # 	if (num in row_list):
